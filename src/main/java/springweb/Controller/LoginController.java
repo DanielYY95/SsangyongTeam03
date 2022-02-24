409b2188bc7ac130553a7a72e232c8499128f9e0
hyoniii710 <89337508+hyoniii710@users.noreply.github.com>
2022-02-24 22:30:44 +0900

    Update LoginController.java

--- src/main/java/springweb/Controller/LoginController.java
+++ src/main/java/springweb/Controller/LoginController.java
@@ -24,10 +24,10 @@ public class LoginController {
 	
 	@Autowired
 	private MembersService service;
-	//(2)세션 유지 페이지 
+	
 	@RequestMapping(params="method=session")
-	//@RequestMapping("/login.do")
-	//// http://localhost:7090/springweb/web.do?method=session
+	//(2)세션 유지 페이지 
+	// http://localhost:7090/springweb/web.do?method=session
 	public String login(@ModelAttribute("members") Members sch, Model d
 			) {
 		System.out.println( "1번 :"+ sch + sch.getmId());sch = service.login(sch);System.out.println( "2번 :"+sch);
@@ -35,16 +35,17 @@ public class LoginController {
 		if(sch!=null) {
 			d.addAttribute("loginMsg","로그인 성공!");
 			d.addAttribute("members",sch);
+			return "/main.do";
+			// 수정 (김효은) : 로그인 성공시 메인화면으로 이동 
 		}else {
 			d.addAttribute("loginMsg","로그인 실패~");
 		}
-//		System.out.println("아이디 : "+sch.getmId());
-//		System.out.println("비밀번호 : "+sch.getmPass());
+
 		
 		return "forward:/web.do?method=login";
 	}
-	
-	// http://localhost:7090/springweb/web.do?method=login (1)메인페이지 
+	// (1) 로그인  페이지 
+	// http://localhost:7090/springweb/web.do?method=login
 	@RequestMapping(params="method=login")
 		public String golog() {
 			return "/WEB-INF/views/jsp/login.jsp";
