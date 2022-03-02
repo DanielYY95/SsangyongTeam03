f7235391a3711dd00f7180677338a7a36bad689d
hyoniii710 <89337508+hyoniii710@users.noreply.github.com>
2022-03-02 21:16:23 +0900

    Add files via upload

---
+++ src/main/java/springweb/MembersDao.java
@@ -0,0 +1,17 @@
+package springweb.DAO;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import springweb.vo.Members;
+
+@Repository
+public interface MembersDao {
+	public List<Members> getMembersList(Members sch);
+	public Members reg(Members reg);
+	public void insertMembers (Members ins);
+	public Members login(Members login);
+	public void updateMypage(Members upt);
+	public int idChk(Members ins);
+}
