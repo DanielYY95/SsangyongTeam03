1c4957f633eeb47bf36ec7823d0c3537698531d9
hyoniii710 <89337508+hyoniii710@users.noreply.github.com>
2022-03-02 21:15:07 +0900

    Update LoginController.java

--- src/main/java/springweb/Controller/LoginController.java
+++ src/main/java/springweb/Controller/LoginController.java
@@ -1,55 +1,83 @@
 package springweb.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import springweb.Service.MembersService;
 import springweb.vo.Members;
 
-@SessionAttributes("members")
 @Controller
-@RequestMapping("/web.do")
 public class LoginController {
-
-	@ModelAttribute("members")
-	public Members getMember() {
-		return new Members();
-	}
-	
 	@Autowired
 	private MembersService service;
 	
-	@RequestMapping(params="method=session")
-	//(2)세션 유지 페이지 
-	// http://localhost:7090/springweb/web.do?method=session
-	public String login(@ModelAttribute("members") Members sch, Model d
-			) {
-		System.out.println( "1번 :"+ sch + sch.getmId());sch = service.login(sch);System.out.println( "2번 :"+sch);
-		// 
-		if(sch!=null) {
-			d.addAttribute("loginMsg","로그인 성공!");
-			d.addAttribute("members",sch);
-			return "/main.do";
-			// 수정 (김효은) : 로그인 성공시 메인화면으로 이동 
-		}else {
-			d.addAttribute("loginMsg","로그인 실패~");
-		}
+	@RequestMapping("/loginFrm.do") 
+	public String loginFrm() {
 
-		
-		return "forward:/web.do?method=login";
+		return "/WEB-INF/views/login_reg_main/login.jsp";
 	}
-	// (1) 로그인  페이지 
-	// http://localhost:7090/springweb/web.do?method=login
-	@RequestMapping(params="method=login")
-		public String golog() {
-			return "/WEB-INF/views/jsp/login.jsp";
+	
+	@RequestMapping("/logout.do") 
+	public String loginout(HttpSession session) {
+
+		// 1. 세션 종료
+		session.invalidate();
+		
+		// 2. 세션 값 변경
+		// d.addAttribute("members", new Members()); // 로그인한 요청값을 받을 수 있게 vo객체 만들어준다. 
+		
+		return "forward:/main.do";
+	}
+	
+	
+	
+	@RequestMapping("/regchk.do")
+	public String goRegchk() {
+		return "/WEB-INF/views/login_reg_main/regcheck.jsp";
+	}
+	
+	
+	//회원가입 form
+		@RequestMapping("/reginsert.do")
+		public String insertMembers(@ModelAttribute Members ins, Model d) {
+			int result = service.idChk(ins);
+			if(result == 1) {
+				d.addAttribute("result", service.idChk(ins));
+				return "/WEB-INF/views/login_reg_main/reg.jsp";
+			} else if(result == 0) {
+				service.insertMembers(ins);
+				d.addAttribute("result", service.idChk(ins));
+			}
+			System.out.println("아이디 : "+ins.getmId());
+			System.out.println("비밀번호 : "+ins.getmPass());
+//			service.insertMembers(ins);
+			
+			return "/WEB-INF/views/login_reg_main/main.jsp";
 		}
 
-}
-
+	//회원가입 페이지 접속
+	@RequestMapping("/reg.do")
+	public String goreg() {
+		
+		return "/WEB-INF/views/login_reg_main/reg.jsp";
+	}
+	
+	// id 중복확인  mId 
+	@ResponseBody
+	@RequestMapping("/regcheck.do")
+	public String idChk(Members ins, Model d) {
+		System.out.println("확인 : "+ins.getmId());
+		d.addAttribute("result", service.idChk(ins));
+		return "pageJsonReport";
+		}
+	}
